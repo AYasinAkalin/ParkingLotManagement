368409343292715b1619a5b014cf1eb957d747c9
uygar <uygarcankat@sabanciuniv.edu>
2019-04-26 22:14:31 +0300

    Update __main__.py
    Install dependencies before executing the program

--- src/__main__.py
+++ src/__main__.py
@@ -2,6 +2,8 @@
 import subprocess as sp
 import definitions
 definitions.init()
+cmd =  'pip install -r requirements.txt'# + str(definitions.FILE_REQS)
+sp.run(cmd, shell=True)
 if definitions.DEBUG:
     sp.run('export FLASK_DEBUG=1', shell=True)
 sp.run('export FLASK_APP=app.py', shell=True)
