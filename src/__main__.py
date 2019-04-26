061276dbbf7d2ae05b5363f738b240c85ede8657
uygar <uygarcankat@sabanciuniv.edu>
2019-04-26 23:14:22 +0300

    Update database.py
    connectedfunction is added

--- src/__main__.py
+++ src/__main__.py
@@ -5,7 +5,7 @@ import database
 definitions.init()
 cmd =  'pip install -r requirements.txt'# + str(definitions.FILE_REQS)
 sp.run(cmd, shell=True)
-database.init()
+database.init("parkinglot.db")
 if definitions.DEBUG:
     sp.run('export FLASK_DEBUG=1', shell=True)
 sp.run('export FLASK_APP=app.py', shell=True)
--- src/database.py
+++ src/database.py
@@ -1,3 +1,13 @@
 import sqlite3
-def init():
-    pass 
+def init(file):
+    c = open(file)
+    c.close()
+
+
+def open(file):
+    conn = sqlite3.connect(file)
+    return conn
+    
+
+
+
