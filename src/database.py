fdf7ef2c9c18573020f9307878d1318bb243acb0
A. Yasin Akalin <ayasinakalin@gmail.com>
2019-05-01 00:29:46 +0300

    Update database.py
    Add tables 'Users', 'UserPermissions', 'Permissions'

--- src/database.py
+++ src/database.py
@@ -1,6 +1,45 @@
 import sqlite3
 def init(file):
     c = open(file)
+    users_sql = "CREATE TABLE Users\
+        (\
+            'UserID'    TEXT\
+                PRIMARY KEY\
+                    ASC\
+                    ON CONFLICT REPLACE\
+                UNIQUE\
+                    ON CONFLICT REPLACE\
+                NOT NULL,\
+            'UserName'  TEXT\
+                UNIQUE\
+                    ON CONFLICT REPLACE\
+                NOT NULL,\
+            'EMail'     TEXT\
+                UNIQUE\
+                    ON CONFLICT ABORT,\
+            'Password'  TEXT\
+        )"
+    execute(users_sql, c)
+    permissions_sql = "CREATE TABLE Permissions\
+        (\
+            'PermissionKey' TEXT\
+                PRIMARY KEY,\
+            'Description'   TEXT\
+        )"
+    execute(permissions_sql, c)
+    user_permissions_sql = "CREATE TABLE UserPermissions\
+        (\
+            'UserID'        TEXT\
+                PRIMARY KEY\
+                    ASC\
+                    ON CONFLICT REPLACE\
+                REFERENCES Users(UserID)\
+                NOT NULL,\
+            'PermissionKey' TEXT\
+                REFERENCES Permissions(PermissionKey)\
+                NOT NULL\
+        )"
+    execute(user_permissions_sql, c)
     c.close()
 
 
