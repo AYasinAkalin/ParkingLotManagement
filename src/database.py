0f38b6e8f0bb4c862b29a1cacebed481396a56f8
A. Yasin Akalin <ayasinakalin@gmail.com>
2019-05-01 01:33:43 +0300

    Update database.py
    Add tables 'Members', 'WalletAccounts', 'CreditCards', 'CardQuartets'

--- src/database.py
+++ src/database.py
@@ -40,6 +40,70 @@ def init(file):
                 NOT NULL\
         )"
     execute(user_permissions_sql, c)
+    members_sql = "CREATE TABLE Members\
+        (\
+            'MemberID'      TEXT\
+                PRIMARY KEY\
+                    ASC\
+                    ON CONFLICT ROLLBACK\
+                UNIQUE\
+                NOT NULL,\
+            'UserID'        TEXT\
+                REFERENCES Users(UserID)\
+                NOT NULL,\
+            'MemberName'    TEXT\
+                NOT NULL\
+        )"
+    execute(members_sql, c)
+    wallet_accounts_sql = "CREATE TABLE WalletAccounts\
+        (\
+            'WalletID'      TEXT\
+                PRIMARY KEY\
+                    ASC\
+                    ON CONFLICT ROLLBACK\
+                UNIQUE\
+                    ON CONFLICT ROLLBACK\
+                NOT NULL,\
+            'MemberID'      TEXT\
+                REFERENCES Members(MemberID)\
+                UNIQUE\
+                NOT NULL\
+        )"
+    execute(wallet_accounts_sql, c)
+    credit_cards_sql = "CREATE TABLE CreditCards\
+        (\
+            'CardID'        TEXT\
+                PRIMARY KEY\
+                    ON CONFLICT ROLLBACK\
+                UNIQUE\
+                    ON CONFLICT ROLLBACK\
+                NOT NULL,\
+            'WalletID'      TEXT\
+                REFERENCES WalletAccounts(WalletID),\
+            'HolderName'    TEXT\
+                NOT NULL,\
+            'ValidUntil'    TEXT\
+                NOT NULL\
+        )"
+    execute(credit_cards_sql, c)
+    card_quartets_sql = "CREATE TABLE CardQuartets\
+        (\
+            'CardID'        TEXT\
+                PRIMARY KEY\
+                    ASC\
+                    ON CONFLICT REPLACE\
+                REFERENCES CreditCards(CardID)\
+                NOT NULL,\
+            'CardQ1'        TEXT\
+                NOT NULL,\
+            'CardQ2'        TEXT\
+                NOT NULL,\
+            'CardQ3'        TEXT\
+                NOT NULL,\
+            'CardQ4'        TEXT\
+                NOT NULL\
+        )"  # Save 1st, 2nd, 3rd quartets encrypted, leave 4th as it is
+    execute(card_quartets_sql, c)
     c.close()
 
 
