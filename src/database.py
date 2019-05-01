bc98c710acc1e61c34aa7b71f866a6f3fff6c4c0
A. Yasin Akalin <ayasinakalin@gmail.com>
2019-05-01 16:11:46 +0300

    Update database.py for compliance with PEP 8

--- src/database.py
+++ src/database.py
@@ -1,18 +1,9 @@
 import sqlite3
+
+
 def init(file):
     c = open(file)
-
-    # Create Boat table
-    boat_sql = "CREATE TABLE `boats` (\
-    `bid` INTEGER NOT NULL,\
-    `bname` TEXT NOT NULL,\
-    `color` TEXT NOT NULL DEFAULT 'white',\
-    PRIMARY KEY (`bid`));"
-    
-    #execute(boat_sql, c)
-
-    #c.close()
-    #create Firm table
+    # create Firm table
     Firm_sql = "CREATE TABLE Firm(\
         'FirmAlias' TEXT NOT NULL UNIQUE,\
         'FirmNAME' TEXT  NOT NULL,\
@@ -24,12 +15,9 @@ def init(file):
         'Region' TEXT UNIQUE,\
         'PostalCode' TEXT UNIQUE,\
         PRIMARY KEY ('FirmAlias'))"
-
     execute(Firm_sql, c)
 
-    #c.close()
-    
-      #create ParkingLots
+    # create ParkingLots
     ParkingLots_sql = "Create Table ParkingLots(\
         'LotAlias' TEXT NOT NULL UNIQUE,\
         'FirmAlias' TEXT UNIQUE,\
@@ -44,11 +32,9 @@ def init(file):
         CONSTRAINT FK_TO_FIRM\
             FOREIGN KEY (FirmAlias)\
             REFERENCES Firm(FirmAlias))"
-
     execute(ParkingLots_sql, c)
-      
-    #c.close()
-      #create taable Floors 
+
+    # create table Floors
     Floors_sql = "CREATE TABLE Floors(\
         'FloorNumber'   TEXT NOT NULL,\
         'LotAlias'      TEXT\
@@ -57,9 +43,9 @@ def init(file):
         PRIMARY KEY('FloorNumber', 'LotAlias')\
         )"
 
-    execute(Floors_sql,c)
+    execute(Floors_sql, c)
 
-     #create table RentalAreas
+    # create table RentalAreas
     Rental_Areas_sql = "CREATE TABLE RentalAreas(\
        'RentalID' TEXT UNIQUE,\
        'FloorNumber'   TEXT NOT NULL\
@@ -67,11 +53,9 @@ def init(file):
         'LotAlias' TEXT UNIQUE\
         REFERENCES Floors(LotAlias),\
         PRIMARY KEY(FloorNumber, LotAlias, RentalID))"
-       
-    execute(Rental_Areas_sql,c)
-   
+    execute(Rental_Areas_sql, c)
 
-       #create table ChargeSpots
+    # create table ChargeSpots
     Charge_spots_sql = "CREATE TABLE ChargeSpots(\
        'CSpotID' TEXT NOT NULL,\
        'LotAlias' TEXT UNIQUE\
@@ -79,34 +63,27 @@ def init(file):
         'Floornumber' TEXT UNIQUE\
          REFERENCES Floors(Floornumber),\
         PRIMARY KEY('CSpotID','LotAlias'))"
+    execute(Charge_spots_sql, c)
 
-    execute(Charge_spots_sql,c)
-           
-        #create table ParkingSpots
+    # create table ParkingSpots
     Parking_Spots_sql = "CREATE TABLE ParkingSpots(\
         'PSpotID' TEXT NOT NULL PRIMARY KEY,\
         'LotAlias' TEXT UNIQUE\
          REFERENCES Chargespots(LotAlias),\
          'FloorNumber'   TEXT NOT NULL\
         REFERENCES Floors(Floornumber))"
-      
+    execute(Parking_Spots_sql, c)
 
-    execute(Parking_Spots_sql,c)
-
-
-         #create table ReservedSpots
+    # create table ReservedSpots
     Reserved_Spots_sql = "CREATE TABLE ReservedSpots(\
         'PSpotID' TEXT NOT NULL\
          REFERENCES ParkingSpots(PSpotID),\
          'MShipNum'  TEXT NOT NULL\
         REFERENCES MembershipRentals(MshipNum),\
         PRIMARY KEY('PSpotID','MShipNum'))"
-    
-    execute(Reserved_Spots_sql,c)
+    execute(Reserved_Spots_sql, c)
 
-
-          
-          #create table Rentalagreements
+    # create table RentalAgreements
     Rental_Agreement_sql = "CREATE TABLE RentalAgreement(\
          'RentalID' TEXT NOT NULL\
          REFERENCES RentalAreas(RentalID),\
@@ -121,40 +98,32 @@ def init(file):
          'Description' TEXT NOT NULL,\
          PRIMARY KEY(RentalID ,'LotAlias','TenandID'))"
 
-    execute(Rental_Agreement_sql,c)
+    execute(Rental_Agreement_sql, c)
 
-           
-
-         #create table TenantContacts
+    # create table TenantContacts
     Tenant_Contacts_sql = "CREATE TABLE TenantContacts(\
           'TenantID' TEXT NOT NULL PRIMARY KEY,\
            'Name'    TEXT NOT NULL,\
             'Telephone' TEXT NOT NULL,\
             'EMail'  TEXT NOT NULL)"
-           
-    execute(Tenant_Contacts_sql,c)
+    execute(Tenant_Contacts_sql, c)
 
-
-        #create table Charginginfo
-    Charging_info_sql = "CREATE TABLE Charginginfo(\
+    # create table ChargingInfo
+    Charging_info_sql = "CREATE TABLE ChargingInfo(\
                   'CSpotID' TEXT NOT NULL PRIMARY KEY\
                     REFERENCES ChargeSpots(CSpotID),\
                     'StartedAt'    TEXT NOT NULL,\
                     'CPercentage' TEXT NOT NULL,\
-                    'Cpower' TEXT NOT NULL,\
+                    'CPower' TEXT NOT NULL,\
                     'ChargeAt'  TEXT NOT NULL)"
-                    
-    execute(Charging_info_sql,c)
+    execute(Charging_info_sql, c)
 
-
-                     
-         #create table Parkinginfo
+    # create table ParkingInfo
     Parking_info_sql = "CREATE TABLE Parkinginfo(\
                     'PSpotID' TEXT NOT NULL PRIMARY KEY\
                     REFERENCES ParkingSpots(PSpotID),\
                     'StartedAt' TEXT NOT NULL)"
-
-    execute(Parking_info_sql,c)
+    execute(Parking_info_sql, c)
 
     users_sql = "CREATE TABLE Users\
         (\
@@ -340,11 +309,9 @@ def init(file):
 def open(file):
     conn = sqlite3.connect(file)
     return conn
-    
 
 
-def execute(sql,conn):
-   cursor = conn.cursor()
-   cursor.execute(sql) 
-   conn.commit()
-
+def execute(sql, conn):
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
